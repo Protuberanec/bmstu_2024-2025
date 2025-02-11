5be5a9bbc6f2e424cc8185d2bb2d80a3b1294f50
Denis <ddd@ddd.com>
2025-02-11 15:07:54 +0300

    with numbers

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include <iostream>
 
 
-int sum(char a, char b) {
+unsigned int sum(char a, char b) {
 	return a + b;
 }
 
 int main(int argc, char** argv) {
 
-	char a = 140;
-	char b = 150;
-	char c = a + b;
+	unsigned char a = 140;
+	unsigned char b = 150;
+	unsigned char c = a + b;
 
 	std::cout << (int)a << " + " << (int)b << " = " << (int)c << std::endl;
-	std::cout << sum(a,b) << std::endl;
+	std::cout << std::hex << sum(a,b) << std::endl;
 	return 0;
 }
