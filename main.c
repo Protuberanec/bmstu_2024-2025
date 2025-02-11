2e1c72573f991c25309ec3a7ca140cb46e194a43
Denis <ddd@ddd.com>
2025-02-11 15:29:04 +0300

    watch and fill elements

--- main.c
+++ main.c
@@ -2,18 +2,27 @@
 
 void ShowElement(const int* addr_0, unsigned int size) {
 	for (int i = 0; i < size; i++) {
-		std::cout << addr_0[i] << "\t";
 //		std::cout << *(addr_0 + i) << "\t";
 //		std::cout << *(addr_0++) << "\t";
+		if (i % 10 == 0 && i != 0) {
+			std::cout << "\n";
+		}
+		std::cout << addr_0[i] << "\t";
 	}
 	std::cout << std::endl;
+}
 
+void fillArray(int *ar, size_t size, int val) {
+	for (int i = 0; i < size; ar[i++] = val) {
+	}
 }
 
 
 int main(int argc, char** argv) {
 
 	int ar[100];
+	fillArray(ar, 100, 0);
+	fillArray(&ar[50], 50, 5);
 	ShowElement(&ar[0], 100);
 	return 0;
 }
