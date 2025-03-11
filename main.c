c9ce73c0380a8a594234742d3706ede8c5b251ad
Denis <ddd@ddd.com>
2025-03-11 14:38:42 +0300

    switch.... and other....

--- main.c
+++ main.c
@@ -5,72 +5,57 @@
 
 //УКАЗАТЕЛИ НА ФУНКЦИИ!!!
 
+enum class SortType {
+	no_sort = 0,
+	Ascending = 1,
+	Descending = 2,
+
+};
+enum STATE_MACHINE {
+	Blink = 0,
+	TurnOff = 1,
+	TurnOn = 2,
+};
+
+#define Blink	0
+#define	TurnOn	1
+#define TurnOff	2
 
 
-int getA(int) {
-	std::cout << __func__ << std::endl;
-	return 'a';
-}
 
-int getB(int) {
-	std::cout << __func__ << std::endl;
-	return 'b';
-}
+void bubbleSort(int*, int, SortType isAsc) {}
 
-int getC(int) {
-	std::cout << __func__ << std::endl;
-	return 'c';
-}
+void test1() {}
+void test2() {}
+void test3() {}
 
-void bubbleSort(int* data, int size, bool (*comp)(int a, int b)) {
-
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size - 1; j++) {
-			if (comp(data[j], data[j+1])) {
-				std::swap(data[j], data[j+1]);
-			}
-		}
-	}
-}
-
-void bubbleSort(int* data, int size) {
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size - 1; j++) {
-			if (data[j] > data[j+1]) {
-				std::swap(data[j], data[j+1]);
-			}
-		}
-	}
-}
-
-bool my_tst_xz(int a, int b) {
-	return a < b;
-}
-
-void test_bubbleSort() {
-	int* ar = new int [10] {9,4,8,10,-1,0,4,7,9,1};
-	bubbleSort(ar, 10);
-
-	bubbleSort(ar, 10, my_tst_xz);
-
-	bubbleSort(ar, 10, [](int a, int b){
-			return (a & 0x01) && (a > b);
-			});
+void (*pTest[3])() = {test1, test2, test3};
 
 
-	return;
-}
 
 int main(int argc, char** argv) {
 
-	test_bubbleSort();
+	bubbleSort(nullptr, 0, SortType::Ascending);
 
-	int (*pget[3])(int) = {getA, getB, getC};
+	STATE_MACHINE t1;
+	pTest[t1]();
 
-	for (int i = 0; i < 3; i++) {
-		std::cout << (char)pget[i](0) << std::endl;
+
+	switch(t1) {
+		case STATE_MACHINE::Blink :
+			break;
+		case STATE_MACHINE::TurnOff :
+			break;
+
+		case STATE_MACHINE::TurnOn :
+			break;
+
+		default:
+			break;
+
+
+
 	}
-
 
 
 	return 0;
