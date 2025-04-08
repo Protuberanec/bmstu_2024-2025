2d1313ac8e3b81239e0d12f229a55e3697a859b5
Denis <ddd@ddd.com>
2025-04-08 15:17:31 +0300

    ooooxxxx

--- main.cpp
+++ main.cpp
@@ -1,58 +1,154 @@
-#include <assert.h>
 #include <iostream>
-#include <string>
-#include <time.h>
-#include <cstring>
 
-#include "double_ll.h"
+void ShowNum(int a) {
+	if (a < 0) {
+
+		return;
+	}
+
+	ShowNum(--a);
+	std::cout << a + 1 << "\t";
+}
+
+int GetMax1(const int* ar, int size) {
+	int left = 0;
+	int right = size - 1;
+
+	while(left != right) {
+		if (ar[left] > ar[right]) {
+			right--;
+			continue;
+		}
+		left++;
+	}
+
+	return ar[left];
+}
+
+int GetMax2(const int* ar, int size) {
+	if (size == 1) {
+		return ar[0];
+	}
+
+	if (ar[0] > ar[size - 1]) {
+		return GetMax2(ar, size - 1);
+	}
+	else {
+		return GetMax2(&ar[1], size - 1);
+	}
+}
+
+int GetMax(const int* ar, int size) {
+	return (size != 1 ) ? ((ar[0] > ar[size - 1]) ? GetMax(ar, size-1) : GetMax(&ar[1], size-1)) : ar[0];
+}
+
+
+int BinarySearch(const int* ar, int size, int element) {
+	if (size <= 1) {
+		if (ar[0] == element) {
+			return size/2;
+		}
+		return -1;
+	}
+
+	if (element > ar[(size)/2]) {
+		int res = BinarySearch(&ar[size / 2], size - size/2, element);
+		res = (res == -1) ? -1 : res + (size) / 2;
+		return res;
+	}
+	else if (element < ar[(size)/2]) {
+		return BinarySearch(&ar[0], size/2, element);
+	}
+
+	return (size)/2;
+}
+
+int test_BinarySearch() {
+	int count_test = 1;
+	std::cout << "------\n" << __func__ << std::endl;
+	{
+		int* ar = new int [10] {1,2,3,4,5,6,7,8,9,10};
+		for (int i = 1; i < 11; i++) {
+			if (BinarySearch(ar, 10, i) == i - 1) 
+				std::cout << "TEST " << count_test++ << " PASSED" << std::endl;
+			else 
+				std::cout << "TEST " << count_test++ << " BAD" << std::endl;
+		}
+		delete [] ar;
+	}
+	{
+		std::cout << "---" << std::endl;
+		int* ar = new int [10] {1,3,3,4,5,6,8,8,9,10};
+		if (BinarySearch(ar, 10, 14) == -1) 
+				std::cout << "TEST " << count_test++ << " PASSED" << std::endl;
+			else 
+				std::cout << "TEST " << count_test++ << " BAD" << std::endl;
+		if (BinarySearch(ar, 10, 0) == -1) 
+				std::cout << "TEST " << count_test++ << " PASSED" << std::endl;
+			else 
+				std::cout << "TEST " << count_test++ << " BAD" << std::endl;
+		if (BinarySearch(ar, 10, 7) == -1) 
+				std::cout << "TEST " << count_test++ << " PASSED" << std::endl;
+			else 
+				std::cout << "TEST " << count_test++ << " BAD" << std::endl;
+
+		if (BinarySearch(ar, 10, 2) == -1) 
+				std::cout << "TEST " << count_test++ << " PASSED" << std::endl;
+			else 
+				std::cout << "TEST " << count_test++ << " BAD" << std::endl;
+		delete [] ar;
+	}
+}
+
+void test_GetMax() {
+	{
+		int* ar = new int [10] {1,2,3,4,5,6,7,8,9,10};
+		if (GetMax(ar, 10) == 10) {
+			std::cout << "TEST PASSED" << std::endl;
+		}
+		else {
+			std::cout << "TEST BAD" << std::endl;
+		}
+		delete [] ar;
+	}
+
+	{
+		int* ar = new int [10] {22,2,3,4,5,6,7,8,9,10};
+		if (GetMax(ar, 10) == 22) {
+			std::cout << "TEST PASSED" << std::endl;
+		}
+		else {
+			std::cout << "TEST BAD" << std::endl;
+		}
+		delete [] ar;
+	}
+	{
+		int* ar = new int [10] {22,2,3,4,40,6,7,8,9,10};
+		if (GetMax(ar, 10) == 40) {
+			std::cout << "TEST PASSED" << std::endl;
+		}
+		else {
+			std::cout << "TEST BAD" << std::endl;
+		}
+		delete [] ar;
+	}
+	{
+		int* ar = new int [10] {5,5,5,5,5,5,5,5,5,5};
+		if (GetMax(ar, 10) == 5) {
+			std::cout << "TEST PASSED" << std::endl;
+		}
+		else {
+			std::cout << "TEST BAD" << std::endl;
+		}
+
+		delete [] ar;
+	}
+}
 
 int main(int argc, char** argv) {
-	LL* my_ll;
-	my_ll = new LL();
 
-	Data stupid_data;
-
-	for (int i = 0; i < 10; i++) {
-		my_ll->putToBegin(stupid_data);
-	}
-
-	for (int i = 0; i < 10; i++) {
-		my_ll->putToEnd(stupid_data);
-	}
-
-	my_ll->printLL();
-
-	bool ok = false;
-	for (int i = 0; i < 3; i++) {
-		my_ll->getFromBegin(ok);
-		if (ok == false) {
-			break;
-		}
-	}
-
-	my_ll->printLL();
-
-	for (int i = 0; i < 3; i++) {
-		my_ll->getFromEnd(ok);
-		if (ok == false) {
-			break;
-		}
-	}
-
-	my_ll->printLL();
-	my_ll->delete_element(5);
-	my_ll->printLL();
-
-	my_ll->delete_element(0);
-	my_ll->printLL();
-
-	std::cout << my_ll->totalCountNodes() << std::endl;
-	my_ll->delete_element(my_ll->totalCountNodes() - 1);
-	my_ll->printLL();
-
-	my_ll->delete_element(1200);
-
-	delete my_ll;
+	test_GetMax();
+	test_BinarySearch();
 
 
 	return 0;
