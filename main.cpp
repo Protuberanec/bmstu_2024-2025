8e6f08e2cddb12478d20fce28b38b52589109294
Denis <ddd@ddd.com>
2025-04-29 17:54:42 +0300

    xz

--- main.cpp
+++ main.cpp
@@ -1,87 +1,69 @@
 #include <iostream>
 #include <vector>
 #include <array>
+#include <map>
+#include <string>
 #include "test.h"
 
-
-
-template<class T> 
-class MyArray {
-	private :
-		T* array;
-		int size;
-		int count_data;
-	public :
-		MyArray()  {
-			array = new T [64];
-			size = 64;
-			count_data = 0;
-		}
-		MyArray(int real_size) {
-			array = new T [real_size];
-			size = real_size;
-			count_data = 0;
-		}
-		~MyArray() {
-			delete [] array;
-		}
-
-		T at(int index) const { return array[index % size];}
-
-		T& operator[](int index) {
-			return array[index % size];
-		}
-		bool operator==(const MyArray<T>& comp) {
-			if (comp.sizeAr() != this->sizeAr()) {
-				return false;
-			}
-			bool ok = true;
-			for (int i = 0; i < comp.sizeAr(); ((comp.at(i) != this->at(i)) ? ok = false : ok = ok), i++) {
-				if (ok == false) {
-					break;
-				}
-			}
-			
-		
-			return ok;
-		}
-
-		int sizeAr() const {
-			return size;
-		}
-
-		void show() {
-			for (int i = 0; i < size; std::cout << array[i++] << " ");	
-			std::cout << std::endl;
-		}
+template<class T, class P>
+struct Pair {
+	T key;
+	P value;
 };
-template<class T>
-std::ostream& operator<<(std::ostream& out, const class MyArray<T>& new_array) {
-	out << "MyArray : \n";
-	for (int i = 0; i < new_array.sizeAr(); out << new_array.at(i) << ((!((i+1) % 8) && i > 0) ? "\n" : "\t"), ++i);
 
-	out << std::endl;
-	
-	return out;
-}
+struct Pair1 {
+	int key;
+	std::string value;
+};
 
-struct Node {};
 
-std::ostream& operator<<(std::ostream& out, const Node& temp) {
+std::pair<int, int> getErrors() { }
+void getErrro1(int& a, int& b) {}
 
-	return out;
-}
 
 
 int main(int argc, char** argv) {
 
-	MyArray<int> test1;
-	MyArray<int> test;
-	for (int i = 0; i < test.sizeAr()*4; test[i] += (i) + 1, i++);
-	for (int i = 0; i < test1.sizeAr()*3; test1[i] += i + 1, i++);
-	std::cout << (test1 == test ? "true" : "false")  << std::endl;
+	std::map<std::string, int> words;
 
-	std::cout << test << std::endl;
+	std::map<int /*parents*/, std::vector<int> /*childsssssss*/> graph;
+	
+	auto show_words = [&]() {
+		for (const auto &[key, value] : words) {
+			std::cout << key << " " << value << std::endl;
+		}
+	};
+
+	
+	std::vector<std::pair<std::string, int>> test_words;
+
+	words["cat"] = 1;
+	for (int i = 0; i < 10; i++) {
+		words["cat"]++;
+	}
+
+	words.insert({"dog", 10});
+	words.insert({"dog", 11});
+	words.insert({"dog", 12});
+	words.insert({"dog", 13});
+	std::cout << words["cat"] << std::endl;
+	std::cout << words.count("cat") << " " << words.count("dog") << std::endl;
+	show_words();
+
+	for (auto iter = words.begin(); iter != words.end(); iter++) {
+		std::cout << iter->first << " " << iter->second << std::endl;
+	}
+
+	for (const auto &[key, value] : words) {
+		std::cout << key << " " << value << std::endl;
+	}
+
+
+	for (;!words.empty();) {
+		words.erase(words.begin());
+	}
+
+	show_words();
 
 	return 0;
 }
