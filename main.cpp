d59005e7b994f7939aa65effac7d08c6ec2d42f7
Denis <ddd@ddd.com>
2025-05-06 12:17:10 +0300

    xz???

--- main.cpp
+++ main.cpp
@@ -5,65 +5,105 @@
 #include <string>
 #include "test.h"
 
-template<class T, class P>
-struct Pair {
-	T key;
-	P value;
+#include <tuple>
+
+
+class Graph {
+	private :
+		std::map<int, std::vector<int>> nodes;
+		void show_rec(int parent, const std::map<int,std::vector<int>>& children) const;
+	public :
+		Graph();
+		Graph(const std::string& file_graph);
+
+		~Graph();
+
+		void addNode(int parent, int child);
+		void ShowNodes() const;
+		void SaveToFile(const std::string& fileName);
 };
 
-struct Pair1 {
-	int key;
-	std::string value;
-};
+Graph::Graph() {}
+Graph::Graph(const std::string& fileName) {}
+Graph::~Graph() {}
 
+void Graph::addNode(int parent, int child) {
+	nodes[parent].push_back(child);
+	if (nodes.count(child) == 1) {
+		return;	
+	}
+	nodes[child] = {};
+}
 
-std::pair<int, int> getErrors() { }
-void getErrro1(int& a, int& b) {}
+void Graph::show_rec(int parent, const std::map<int,std::vector<int>>& children) const {
+	std::cout << parent << "{";
+	if (children.at(parent).empty()) {
+		std::cout << "}";
+		return;
+	}
 
+	if (!children.at(parent).empty()) {
+		show_rec(*children.at(parent).begin(), children);
+	}
+	else {
+		std::cout << *children.at(parent).begin();
+	}
+
+	for (auto iter = children.at(parent).begin() + 1; iter != children.at(parent).end(); iter++) {
+		if (!children.at(*iter).empty()) {
+			show_rec(*iter, children);
+			continue;
+		}
+		else {
+			std::cout << ", " << *iter;
+		}
+	}
+	std::cout << "}";
+
+}
+
+void Graph::ShowNodes() const {
+	show_rec(0, nodes);
+	std::cout << std::endl;
+	return ;
+	for (auto iter = nodes.begin(); iter != nodes.end(); iter++) {
+		if (iter->second.empty()) {
+			continue;
+		}
+		std::cout << iter->first << "{" << *iter->second.begin();
+		for (auto iter1 = iter->second.begin() + 1; iter1 != iter->second.end(); iter1++) {
+			std::cout << ", " << *iter1;
+		}
+		std::cout << "}";
+	}
+	std::cout << std::endl;
+}
 
 
 int main(int argc, char** argv) {
 
-	std::map<std::string, int> words;
+	Graph myGraph;
+	myGraph.addNode(0, 1);
+	myGraph.addNode(0, 2);
+	myGraph.addNode(0, 3);
+	myGraph.addNode(0, 4);
+	myGraph.addNode(0, 5);
+	myGraph.addNode(0, 6);
+	myGraph.addNode(0, 7);
+	myGraph.addNode(1, 8);
+	myGraph.addNode(1, 9);
+	myGraph.addNode(1, 10);
+	myGraph.addNode(1, 11);
+	myGraph.addNode(1, 12);
+	myGraph.addNode(2, 13);
+	myGraph.addNode(0, 14);
+	myGraph.addNode(0, 15);
+	myGraph.addNode(0, 16);
+	myGraph.addNode(0, 17);
+	myGraph.addNode(0, 18);
 
-	std::map<int /*parents*/, std::vector<int> /*childsssssss*/> graph;
-	
-	auto show_words = [&]() {
-		for (const auto &[key, value] : words) {
-			std::cout << key << " " << value << std::endl;
-		}
-	};
+	myGraph.ShowNodes();
 
-	
-	std::vector<std::pair<std::string, int>> test_words;
-
-	words["cat"] = 1;
-	for (int i = 0; i < 10; i++) {
-		words["cat"]++;
-	}
-
-	words.insert({"dog", 10});
-	words.insert({"dog", 11});
-	words.insert({"dog", 12});
-	words.insert({"dog", 13});
-	std::cout << words["cat"] << std::endl;
-	std::cout << words.count("cat") << " " << words.count("dog") << std::endl;
-	show_words();
-
-	for (auto iter = words.begin(); iter != words.end(); iter++) {
-		std::cout << iter->first << " " << iter->second << std::endl;
-	}
-
-	for (const auto &[key, value] : words) {
-		std::cout << key << " " << value << std::endl;
-	}
-
-
-	for (;!words.empty();) {
-		words.erase(words.begin());
-	}
-
-	show_words();
 
 	return 0;
 }
